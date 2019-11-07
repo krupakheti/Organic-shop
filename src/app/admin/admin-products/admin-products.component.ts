7f904b16724b4f211038029a240c0bc9e6b4db10
Kundan Rupakheti <kundan.rupakheti99@gmail.com>
2019-11-07 01:01:12 -0600

    admin access is denying again

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,7 +2,7 @@ import { Product } from './../../models/product';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { DataTableResource } from 'angular-4-data-table';
+import { DataTableResource } from 'angular5-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -12,11 +12,11 @@ import { DataTableResource } from 'angular-4-data-table';
 export class AdminProductsComponent implements OnInit, OnDestroy {
   
   products: Product[];
-  filteredProducts: any[];
+  
 
   subscription: Subscription;
   tableResource: DataTableResource<Product>;
-  items: Product[];
+  items: Product[]= [];
   itemCount : number;
 
 
@@ -24,22 +24,39 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
 
   constructor(private ProductService: ProductService) {
      this.subscription =  this.ProductService.getAll().subscribe(products =>{
-              this.filteredProducts = this.products = products;
-              this.tableResource = new DataTableResource(products);
-              this.tableResource.query({offset: 0 })
-                .then(items => this.items = items);
-              this.tableResource.count()
-                .then (count => this.itemCount = count);  // all about initializing the data-table
+             // all about initializing the data-table
+             this.products = this.products = products;
+             this.initializeTable(products);
+
 
      });
          
-
    }
 
+  private initializeTable(products: Product[]){
+    
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then (count => this.itemCount = count); 
+  }
+
+
+  reloadItems(params){
+    if (!this.tableResource) return;
+    this.tableResource.query(params)
+    .then(items => this.items = items);
+   
+
+  }
+
   filter(query: string){
-    this.filteredProducts = (query)?
+    let filteredProducts = (query)?
     this.products.filter(p=> p.title.toLowerCase().includes(query.toLocaleLowerCase())) : 
     this.products;
+
+    this.initializeTable(filteredProducts);
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
-import {DataTableModule} from 'angular-4-data-table';
+import {DataTableModule} from 'angular5-data-table';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import {RouterModule} from '@angular/router';
