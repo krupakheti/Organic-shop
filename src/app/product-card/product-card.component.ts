053d67636370731541ed18f0cfaff57f2e6a55d8
Kundan Rupakheti <kundan.rupakheti99@gmail.com>
2019-11-07 18:56:39 -0600

    sticky categories table when user scrolls

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductCardComponent {
 
   @Input('product') product: Product;
+  @Input('show-actions') showActions = true;
   
+
 
   constructor() { }
 
